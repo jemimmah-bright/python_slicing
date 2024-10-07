6e9620fcf78ccd7994bc42bdfc9b3f89c304093c
jemimmah-bright <jemimmahbright@gmail.com>
2024-10-07 04:25:50 -0700

    my notes

---
+++ slicing.py
@@ -0,0 +1,31 @@
+#to acces a range of characters in a string forexample
+name= "cathy"
+print(name [1:3])
+#we use the start indext and the end index 
+massage="hello"
+print(massage[0:3])
+# when dealing with negative slicing
+#we start from the right
+massage="hello"
+print(massage[-1])
+print(massage[-1])
+print(massage[-1:-5])
+print(massage[-5:-3])
+print(massage[-4:])
+print(massage[:4])
+name="jemimmah"
+age=20
+print(f"my name is {name} and my {age} years old.")
+name ="jemimmah"
+weight = 50
+print(f"my name is {name} and am {weight} kilograms")
+#STRING METHON
+#there are functions usded to manuplilate variables
+name="jemimmah"
+print(len(name))
+total_length= len(name)
+address= "from kamwokya"
+print(len(address))
+name='JEMIMMAH'
+print(len(name))
+name= "Jemimmah\n bright\n"
